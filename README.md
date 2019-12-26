02d2d30085fb5d356a7eeb03a7dae8f4495b3402
zhulu <shaonianzhentan@sina.cn>
2019-12-26 19:38:03 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # lovelace-ha-iframe-panel
-一个在HA里使用的iframe面板
+一个在HA里使用的侧边栏iframe面板
+
+```
+
+# 自定义面板
+panel_custom:
+  # 普通页面（和HA里的panel_iframe功能一样）
+  - name: ha-iframe-panel
+    url_path: ha-iframe-panel-1
+    sidebar_title: 普通页面
+    sidebar_icon: mdi:react
+    js_url: /local/ha-iframe-panel.js
+    config:
+      url: https://github.com/shaonianzhentan/lovelace-ha-iframe-panel
+  # 全屏页面（隐藏头部）
+  # fullscreen 设置为 true
+  - name: ha-iframe-panel
+    url_path: ha-iframe-panel-2
+    sidebar_title: 全屏页面
+    sidebar_icon: mdi:react
+    js_url: /local/ha-iframe-panel.js
+    config:
+      url: https://github.com/shaonianzhentan/lovelace-ha-iframe-panel
+      fullscreen: true
+  # 打开新页面（浏览器新开一个选项卡）
+  # blank 设置为 true
+  - name: ha-iframe-panel
+    url_path: ha-iframe-panel-3
+    sidebar_title: 打开新页面
+    sidebar_icon: mdi:react
+    js_url: /local/ha-iframe-panel.js
+    config:
+      url: https://github.com/shaonianzhentan/lovelace-ha-iframe-panel
+      blank: true
+
+```
---
+++ ha-iframe-panel.js
@@ -0,0 +1,108 @@
+/**
+ * 
+ * 作者QQ：635147515
+ * 日期：2019-12-26
+ * 版本：1.0
+ * 功能：
+ *      1. 在HA里打开新页面
+ *      2. 在HA里全屏显示页面
+ *      3. 在HA里打开页面
+ */
+
+class HaIframePanel extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({ mode: 'open' });
+        const div = document.createElement('div');
+        div.className = 'ha-iframe-panel'
+        div.innerHTML = `
+            <app-toolbar>
+            </app-toolbar>
+            <iframe></iframe>
+        `
+        shadow.appendChild(div)
+
+        const style = document.createElement('style')
+        style.textContent = `
+            app-header, app-toolbar {
+                background-color: var(--primary-color);
+                font-weight: 400;
+                color: var(--text-primary-color, white);
+            }
+            .ha-iframe-panel{    
+                position: relative;
+                width: 100%;
+                overflow: hidden;
+                height: 100vh;
+                z-index: 0;}
+            .ha-iframe-panel iframe{width:100%;height:calc(100vh - 64px);border:none;}
+
+            .ha-iframe-panel.fullscreen app-toolbar{display:none;}
+            .ha-iframe-panel.fullscreen iframe{height:100vh;}
+
+        `
+        shadow.appendChild(style);
+
+        this.shadow = shadow
+
+        // console.log('%O', this)
+    }
+
+    get panel() {
+        return this._panel
+    }
+
+    set panel(value) {
+        this._panel = value
+
+        this.setTitle(value.title)
+        this.init(value.config)
+    }
+
+    init({ url, fullscreen, blank }) {
+        const { shadow } = this
+
+        if (blank) {
+            window.open(url)
+            history.back()
+            return
+        }
+
+        let div = shadow.querySelector('div')
+        if (div.classList.contains('fullscreen')) {
+            if (fullscreen === false) {
+                div.classList.remove('fullscreen')
+            }
+        } else {
+            if (fullscreen) {
+                div.classList.add('fullscreen')
+            }
+        }
+
+
+
+        let iframe = shadow.querySelector('iframe')
+        iframe.src = url
+    }
+
+    set narrow(value){
+        let menuButton = this.shadow.querySelector('ha-menu-button')
+        menuButton.hass = this.hass
+        menuButton.narrow = value
+    }
+
+    setTitle(title) {
+        let toolbar = this.shadow.querySelector('app-toolbar')
+        if (toolbar.children.length === 0) {
+            let menuButton = document.createElement('ha-menu-button')
+            toolbar.appendChild(menuButton)
+
+            let div = document.createElement('div')
+            div.setAttribute('main-title', '')
+            div.textContent = title
+            toolbar.appendChild(div)
+        }
+    }
+}
+
+customElements.define('ha-iframe-panel', HaIframePanel);
